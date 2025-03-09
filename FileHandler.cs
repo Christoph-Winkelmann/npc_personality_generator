ad0c72b9b0b338b00661afd119c7f40897995362
Christoph-Winkelmann <christoph.winkelmann@live.de>
2025-03-09 22:09:58 +0100

    small addendum

--- FileHandler.cs
+++ FileHandler.cs
@@ -4,10 +4,10 @@ namespace npc_generator;
 
 public class FileHandler
 {
-  private const string speciesFilePath = @"C:\Users\Christoph\Documents\c#-projects\npc-generator\species.txt";
-  private const string positiveTraitsFilePath = @"C:\Users\Christoph\Documents\c#-projects\npc-generator\positiveTraits.txt";
-  private const string negativeTraitsFilePath = @"C:\Users\Christoph\Documents\c#-projects\npc-generator\negativeTraits.txt";
-  private const string savedFilePath = @"C:\Users\Christoph\Documents\c#-projects\npc-generator\save.txt";
+  private const string speciesFilePath = "species.txt";
+  private const string positiveTraitsFilePath = "positiveTraits.txt";
+  private const string negativeTraitsFilePath = "negativeTraits.txt";
+  private const string savedFilePath = "save.txt";
 
   public string SpeciesFilePath { get => speciesFilePath; }
   public string PositiveTraitsFilePath { get => positiveTraitsFilePath; }
